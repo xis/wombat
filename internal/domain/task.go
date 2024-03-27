d826bb40948b152f582dbd6ae73da3502ec47665
xis <fopeak@gmail.com>
2024-03-28 00:41:05 +0300

    feat(domain): add task service

---
+++ internal/domain/task.go
@@ -0,0 +1,97 @@
+package domain
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/rs/xid"
+)
+
+var (
+	ErrTaskNotFound   = errors.New("task not found")
+	ErrNoPendingTasks = errors.New("no pending tasks")
+)
+
+type TaskService interface {
+	GetPendingTask(ctx context.Context, params GetPendingTaskParams) (Task, error)
+	UpdateTaskStatus(ctx context.Context, taskID string, status TaskStatus) error
+	CreateTask(ctx context.Context, queueID string, payload []byte) (Task, error)
+}
+
+type GetPendingTaskParams struct {
+	WorkerID string
+	QueueID  string
+}
+
+type TaskRepository interface {
+	GetPendingTask(ctx context.Context, params GetPendingTaskParams) (Task, error)
+	UpdateTaskStatus(ctx context.Context, taskID string, status TaskStatus) error
+	CreateTask(ctx context.Context, task Task) (Task, error)
+	AssignTask(ctx context.Context, taskID, workerID string) error
+}
+
+type TaskStatus string
+
+const (
+	TaskStatusPending    TaskStatus = "pending"
+	TaskStatusProcessing TaskStatus = "processing"
+	TaskStatusCompleted  TaskStatus = "completed"
+)
+
+type Task struct {
+	ID        string
+	QueueID   string
+	Status    TaskStatus
+	CreatedAt time.Time
+	Payload   []byte
+}
+
+type TaskServiceDependencies struct {
+	TaskRepository TaskRepository
+}
+
+type taskService struct {
+	repository TaskRepository
+}
+
+func NewTaskService(dependencies TaskServiceDependencies) TaskService {
+	return &taskService{
+		repository: dependencies.TaskRepository,
+	}
+}
+
+func (s *taskService) GetPendingTask(ctx context.Context, params GetPendingTaskParams) (Task, error) {
+	task, err := s.repository.GetPendingTask(ctx, params)
+	if err != nil {
+		return Task{}, err
+	}
+
+	return task, nil
+}
+
+func (s *taskService) UpdateTaskStatus(ctx context.Context, taskID string, status TaskStatus) error {
+	err := s.repository.UpdateTaskStatus(ctx, taskID, status)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *taskService) CreateTask(ctx context.Context, queueID string, payload []byte) (Task, error) {
+	task := Task{
+		ID:        xid.New().String(),
+		QueueID:   queueID,
+		Status:    TaskStatusPending,
+		CreatedAt: time.Now(),
+		Payload:   payload,
+	}
+
+	_, err := s.repository.CreateTask(ctx, task)
+	if err != nil {
+		return Task{}, err
+	}
+
+	return task, nil
+}
